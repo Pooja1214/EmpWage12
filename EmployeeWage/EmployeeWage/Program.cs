5c7131d3b2e94746ca94e8aae846c325120c9063
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-03-10 17:03:59 +0530

    initial commit

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -5,18 +5,48 @@ Random random = new Random();//use to generate random number
 
 const int present = 1,
           abcent = 0,
-          perHrWage = 20;
+          halfday = 2,
+          partTime = 4,
+          perHrWage = 20,
+          fulltime = 8
+         ;
 
 
-    int isPresent = random.Next(2);//0,1
-Console.WriteLine("is present:" + isPresent);
+int isPresent = random.Next(3),//0 = abcent,1 = fulltime/present, 2 =parttime
+     empWage =0;
+
+switch (isPresent)
+{
+    case present:
+        Console.WriteLine("Employee is present");
+        empWage = perHrWage * fulltime;
+        break;
+    case halfday:
+        Console.WriteLine("Employee is present half day");
+        empWage = perHrWage * partTime;
+        break;
+    default:
+        Console.WriteLine("employee is abcent");
+        break;
+}
+
+
+/*if else code
+Console.WriteLine("is present:" + isPresent); //if else code
 
 if(isPresent == present)
 {
-    Console.WriteLine("Employee is present");
+Console.WriteLine("Employee is present");
+        empWage = perHrWage * fulltime;
+}
+else if (isPresent == halfday)
+{
+    Console.WriteLine("Employee is present half");
+    empWage = perHrWage * partTime;
 }
 else
 {
 
     Console.WriteLine("Employee is Abcent");
-}
+}*/
+Console.WriteLine("employee wage: " + empWage);
