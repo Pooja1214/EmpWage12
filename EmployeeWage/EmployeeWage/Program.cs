d61b059cf8fa4529e6ecad444aca43bc2001317b
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-03-17 14:35:11 +0530

    UC_6 : Checking EmpWage Until Condition Reached

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -10,48 +10,45 @@ namespace EmployeeWage1
             const int WAGE_PER_HOUR = 20;
             const int FULL_TIME_WORKING_HOUR = 8;
             int PART_TIME_WORKING_HOUR = 4;
-            int wage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            if (empCheck == 0)
+
+            int TOTAL_WORKING_HOURS_IN_MONTH = 100;
+            int NO_OF_WORKING_DAY_IN_MONTH = 20;
+
+            //initialize variable
+            int day = 0;
+            int totalWorkingHr = 0;
+            while (day != NO_OF_WORKING_DAY_IN_MONTH && totalWorkingHr != TOTAL_WORKING_HOURS_IN_MONTH)
             {
-                Console.WriteLine("Employee is present");
-                wage = FULL_TIME_WORKING_HOUR * WAGE_PER_HOUR;
-                Console.WriteLine($"WAGE:{wage}");
-            }
-            else if (empCheck == PARTIAL_PRESENT)
-            {
-                Console.WriteLine("Employee is present");
-                wage = FULL_TIME_WORKING_HOUR * WAGE_PER_HOUR;
-                Console.WriteLine($"WAGE:{wage}");
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
+                int wage = 0;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case PRESENT:
+                        Console.WriteLine("Employee is present");
+                        wage = FULL_TIME_WORKING_HOUR * WAGE_PER_HOUR;
+                        totalWorkingHr = totalWorkingHr + FULL_TIME_WORKING_HOUR;
+                        Console.WriteLine($"WAGE:{wage}");
+                        break;
+
+                    case PARTIAL_PRESENT:
+                        Console.WriteLine("Employee is partial present");
+                        wage = PART_TIME_WORKING_HOUR * WAGE_PER_HOUR;
+                        totalWorkingHr = totalWorkingHr + PART_TIME_WORKING_HOUR;
+                        Console.WriteLine($"WAGE:{wage}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        Console.WriteLine($"WAGE:{wage}");
+                        break;
+
+                }
+                day++;
 
             }
-            switch (empCheck)
-            {
-                case PRESENT:
-                    Console.WriteLine("Employee is present");
-                    wage = FULL_TIME_WORKING_HOUR * WAGE_PER_HOUR;
-                    Console.WriteLine($"WAGE:{wage}");
-                    break;
-
-                case PARTIAL_PRESENT:
-                    Console.WriteLine("Employee is present");
-                    wage = PART_TIME_WORKING_HOUR * WAGE_PER_HOUR;
-                    Console.WriteLine($"WAGE:{wage}");
-                    break;
-
-                default:
-                    Console.WriteLine("Employee is absent");
-                    Console.WriteLine($"WAGE:{wage}");
-                    break;
-            }
-
-
-
+            Console.WriteLine($"No of working day:{day} and Total working hr:{totalWorkingHr}");
         }
     }
 }
